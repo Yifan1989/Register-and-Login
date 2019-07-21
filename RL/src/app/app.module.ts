082d840f11f520bc99e8026a2df78970ea5b122f
Yifan1989 <j.codes1989@gmail.com>
2019-07-21 15:42:15 +1200

    routing done

--- RL/src/app/app.module.ts
+++ RL/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { appRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    appRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
